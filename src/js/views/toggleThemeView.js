02987889d1a0894fda472a105cbda68dfce1addf
stormrageqb <stormrage.qb@gmail.com>
2022-11-01 13:09:03 -0500

    Add thematic style system for toggle style theme on default style

--- src/js/views/toggleThemeView.js
+++ src/js/views/toggleThemeView.js
@@ -1,9 +1,11 @@
 class ToggleThemeView {
   _toggleSwitches = document.querySelectorAll('.btn');
-  _toggleSwitch = document.querySelector('.btn');
+  // _toggleSwitch = document.querySelectorAll('.btn');
   _body = document.querySelector('body');
+  _triToggleEl = document.querySelector('.tri-toggle');
   _calcDisplay = document.querySelector('.calc-display');
   _calcInput = document.querySelector('.calc-input-container');
+  _calcBtns = document.querySelectorAll('.calcBtn');
   _calcNumbers = document.querySelector('.calc-numbers');
   _equalsBtn = document.querySelector('.equalsBtn');
   _resetBtn = document.querySelector('.resetBtn');
@@ -15,14 +17,32 @@ class ToggleThemeView {
       el.addEventListener('click', () => {
         el.style.opacity = '1';
         if (el.dataset.id === 'theme2') {
-          this._body.classList.add('body-theme-two');
-          this._calcDisplay.classList.add('calc-display-theme-two');
-          this._calcInput.classList.add('calc-input-theme-two');
-          this._calcNumbers.classList.add('calc-numbers-theme-two');
-          this._toggleSwitch.classList.add('toggle-switches-theme-two');
-          this._equalsBtn.classList.add('equals-btn-theme-two');
-          this._resetBtn.classList.add('reset-btn-theme-two');
-          this._delBtn.classList.add('delete-btn-theme-two');
+          this._body.classList.toggle('body-theme-two');
+          this._calcDisplay.classList.toggle('calc-display-theme-two');
+          this._calcInput.classList.toggle('calc-input-theme-two');
+          this._calcNumbers.classList.toggle('calc-numbers-theme-two');
+          // this._toggleSwitch.classList.toggle('toggle-switches-theme-two');
+          this._equalsBtn.classList.toggle('equals-btn-theme-two');
+          this._resetBtn.classList.toggle('reset-btn-theme-two');
+          this._delBtn.classList.toggle('delete-btn-theme-two');
+        }
+
+        if (el.dataset.id === 'theme3') {
+          this._body.classList.toggle('body-theme-three');
+          this._triToggleEl.classList.toggle('tri-toggle-theme-three');
+          this._calcDisplay.classList.toggle('calc-display-theme-three');
+          this._calcInput.classList.toggle('calc-input-theme-three');
+          this._calcBtns.forEach(btn => {
+            btn.classList.toggle('calc-button-theme-three');
+          });
+          this._calcNumbers.classList.toggle('calc-numbers-theme-three');
+          // this._toggleSwitch.classList.add('toggle-switches-theme-three');
+          // this._toggleSwitch.forEach(btn => {
+          //   btn.classList.add('toggle-switches-theme-three');
+          // });
+          this._equalsBtn.classList.toggle('equals-btn-theme-three');
+          this._resetBtn.classList.toggle('reset-btn-theme-three');
+          this._delBtn.classList.toggle('delete-btn-theme-three');
         }
         arr
           .filter(function (item) {

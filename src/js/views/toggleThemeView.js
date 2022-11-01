668aadea65f70787c5d03ca12da31eeaeb99248b
stormrageqb <stormrage.qb@gmail.com>
2022-11-01 10:32:09 -0500

    Add toggle styles for theme two in toggleThemeView

--- src/js/views/toggleThemeView.js
+++ src/js/views/toggleThemeView.js
@@ -1,18 +1,35 @@
 class ToggleThemeView {
-  _toggleSwitches = document.querySelectorAll(".btn");
+  _toggleSwitches = document.querySelectorAll('.btn');
+  _toggleSwitch = document.querySelector('.btn');
+  _body = document.querySelector('body');
+  _calcDisplay = document.querySelector('.calc-display');
+  _calcInput = document.querySelector('.calc-input-container');
+  _calcNumbers = document.querySelector('.calc-numbers');
+  _equalsBtn = document.querySelector('.equalsBtn');
+  _resetBtn = document.querySelector('.resetBtn');
+  _delBtn = document.querySelector('.delBtn');
   addHandlerRender(handler) {
     const arr = [...this._toggleSwitches];
     console.log(arr);
     arr.forEach((el, index) => {
-      el.addEventListener("click", () => {
-        el.style.opacity = "1";
-
+      el.addEventListener('click', () => {
+        el.style.opacity = '1';
+        if (el.dataset.id === 'theme2') {
+          this._body.classList.add('body-theme-two');
+          this._calcDisplay.classList.add('calc-display-theme-two');
+          this._calcInput.classList.add('calc-input-theme-two');
+          this._calcNumbers.classList.add('calc-numbers-theme-two');
+          this._toggleSwitch.classList.add('toggle-switches-theme-two');
+          this._equalsBtn.classList.add('equals-btn-theme-two');
+          this._resetBtn.classList.add('reset-btn-theme-two');
+          this._delBtn.classList.add('delete-btn-theme-two');
+        }
         arr
           .filter(function (item) {
             return item !== el;
           })
-          .forEach((item) => {
-            item.style.opacity = "0";
+          .forEach(item => {
+            item.style.opacity = '0';
           });
       });
     });

e095e3eb1ab3feb61d7edb36652265775ae09697
stormrageqb <stormrage.qb@gmail.com>
2022-11-02 14:13:14 -0500

    Add new method for obtaining values and save them to empty object of arrays

--- src/js/views/calcDisplayView.js
+++ src/js/views/calcDisplayView.js
@@ -1,8 +1,14 @@
 class CalcDisplayView {
+  _numBtns = document.querySelectorAll('.numBtn');
+  _operatorBtns = document.querySelectorAll('.operator');
   _calcDisplay = document.querySelector('.calc-display');
   _calcDisplayValue = document.querySelector('.calc-numbers');
   _calcInputContainer = document.querySelector('.calc-input-container');
-  _arr = [];
+  _arr = {
+    textValue: [],
+    numbers: [],
+    operators: [],
+  };
 
   findSum(...args) {
     const arr = [];
@@ -44,27 +50,68 @@ class CalcDisplayView {
     return sum;
   }
 
-  compute(operator, num1, num2) {}
+  compute(operator, num1, num2) {
+    if (operator === '+') {
+      return this.findSum(num1, num2);
+    }
+    if (operator === '-') {
+      return this.findDifference(num1, num2);
+    }
+    if (operator === '*') {
+      return this.findProduct(num1, num2);
+    }
+    if (operator === '/') {
+      return this.findQuotient(num1, num2);
+    }
+  }
   addComputationRender() {
-    this._calcInputContainer.addEventListener('click', e => {
-      const calcBtn = e.target.closest('.calcBtn');
-      console.log(calcBtn);
-      //   if (calcBtn.dataset.id === 'reset') {
-      //     this._calcDisplay.innerHTML = '';
-      //   }
-      if (!calcBtn) return;
-      const calcBtnValue = calcBtn.textContent;
-      //   if (calcBtnValue.dataset.id === 'reset') {
-      //     console.log('reset');
-      //   }
-      //   const trimmedValue = calcBtnValue.trim();
-      //   console.log(trimmedValue);
-      //   console.log(calcBtnValue);
-      this._arr.push(calcBtnValue);
-      console.log(this._arr);
-      const markup = `<h2 class='calc-numbers'>${calcBtnValue}</h2>`;
-      this._calcDisplay.insertAdjacentHTML('beforeend', markup);
+    this._numBtns.forEach(btn => {
+      btn.addEventListener('click', () => {
+        if (!btn) return;
+        const calcBtnValue = +btn.textContent;
+        console.log(calcBtnValue);
+        this._calcDisplayValue.insertAdjacentHTML('beforeend', calcBtnValue);
+        this._arr.textValue.push(this._calcDisplayValue);
+        const displayText = this._arr.textValue[0].outerText;
+        const displayNumber = +displayText;
+        console.log(displayNumber);
+        this._arr.numbers.push(displayNumber);
+        console.log(this._arr);
+      });
     });
+
+    this._operatorBtns.forEach(btn => {
+      btn.addEventListener('click', () => {
+        if (!btn) return;
+        const operatorBtnValue = btn.dataset.id.trim();
+        console.log(operatorBtnValue);
+        this._arr.operators.push(operatorBtnValue);
+        console.log(this._arr);
+      });
+    });
+
+    // this._calcInputContainer.addEventListener('click', e => {
+
+    // const calcBtn = e.target.closest('.calcBtn');
+    // const operatorBtn = e.target.closest('.operator');
+
+    // if (!calcBtn) return;
+
+    // const calcBtnValue = calcBtn.textContent;
+    // this._arr.push(calcBtnValue);
+    // console.log(this._arr);
+    // const split = this._arr.toString();
+    // console.log(split);
+    // const fullString = split.replaceAll(',', '');
+    // const fullNum = +fullString;
+    // console.log(fullNum);
+    // if (calcBtnValue === '+') {
+    //   const equation = this.compute('+', fullNum);
+    //   console.log(equation);
+    // }
+    // const markup = `<h2 class='calc-numbers'>${calcBtnValue}</h2>`;
+    // this._calcDisplay.insertAdjacentHTML('beforeend', markup);
+    // });
   }
 }
 

8935ebdeae3ca54f750ea68c15013693f5caf6b8
stormrageqb <stormrage.qb@gmail.com>
2022-11-02 10:02:08 -0500

    Add compute function along with global arr variable to store all button clicks

--- src/js/views/calcDisplayView.js
+++ src/js/views/calcDisplayView.js
@@ -2,6 +2,7 @@ class CalcDisplayView {
   _calcDisplay = document.querySelector('.calc-display');
   _calcDisplayValue = document.querySelector('.calc-numbers');
   _calcInputContainer = document.querySelector('.calc-input-container');
+  _arr = [];
 
   findSum(...args) {
     const arr = [];
@@ -46,22 +47,21 @@ class CalcDisplayView {
   compute(operator, num1, num2) {}
   addComputationRender() {
     this._calcInputContainer.addEventListener('click', e => {
-      const arr = [];
       const calcBtn = e.target.closest('.calcBtn');
       console.log(calcBtn);
       //   if (calcBtn.dataset.id === 'reset') {
       //     this._calcDisplay.innerHTML = '';
       //   }
       if (!calcBtn) return;
-
+      const calcBtnValue = calcBtn.textContent;
       //   if (calcBtnValue.dataset.id === 'reset') {
       //     console.log('reset');
       //   }
       //   const trimmedValue = calcBtnValue.trim();
       //   console.log(trimmedValue);
-      console.log(calcBtnValue);
-      arr.push(calcBtnValue);
-      console.log(arr);
+      //   console.log(calcBtnValue);
+      this._arr.push(calcBtnValue);
+      console.log(this._arr);
       const markup = `<h2 class='calc-numbers'>${calcBtnValue}</h2>`;
       this._calcDisplay.insertAdjacentHTML('beforeend', markup);
     });
